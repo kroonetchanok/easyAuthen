1cd682c994acd36e992b1d7437513eccc0b0ebdd
Student12 <Student12@Student12s-iMac.local>
2017-06-14 10:30:56 +0700

    check User Ok

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Implicit 
+    var strUser: String?
+    var strPassword: String?
+    
+    
+    
+    
     
     @IBOutlet weak var userTextField: UITextField!
     
@@ -23,7 +30,41 @@ class ViewController: UIViewController {
     
     
     @IBAction func loginButtom(_ sender: Any) {
-    }
+    
+    
+    // Get Value From TextField 
+        strUser = userTextField.text
+        strPassword = passwordTextField.text
+        
+        // show User and Pass on Console 
+        print("User ==> \(strUser!)")
+        print("Password ==> \(strPassword!)")
+        
+        // Find Count String
+        let intUser  = strUser?.characters.count
+        let intPass = strPassword?.characters.count
+        
+    
+        
+        print("intUser ==? \(intUser!)")
+        print("intPass ==> \(intPass!)")
+
+        // Call checkspace
+        if checkSpace(myString: strUser!) {
+            print("user OK")
+        }  else {
+            print("Blank User")
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    }  // loginButton
+    
     
     
     
@@ -35,12 +76,30 @@ class ViewController: UIViewController {
         
         
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }  // Main Method
 
+    func checkSpace(myString: String) -> Bool {
+        let intString = myString.characters.count
+        var result: Bool = true
+        
+        if intString == 0 {
+            // No Space
+            result = false
+        }
+        
+        
+        return result
+        
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }  // didRecive
 
 
 }
